e871d88e3ca22a31bc92fa2197cb36ca9a742812
Merlin Hannemann <merlinhannemann@gmx.de>
2018-11-14 16:12:06 +0800

    fixed small migration bug

--- db/migrate/20181114074349_create_lawyer_attributes.rb
+++ db/migrate/20181114074349_create_lawyer_attributes.rb
@@ -2,7 +2,7 @@ class CreateLawyerAttributes < ActiveRecord::Migration[5.2]
   def change
     create_table :lawyer_attributes do |t|
       t.string :attr_type
-      t.references :attr_id, foreign_key: true
+      t.integer :attr_id
       t.references :lawyer, foreign_key: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,100 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_14_012502) do
+ActiveRecord::Schema.define(version: 2018_11_14_074642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "lawyer_id"
+    t.integer "price"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["lawyer_id"], name: "index_bookings_on_lawyer_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "communications", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "conversations", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "languages", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "law_fields", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "lawyer_attributes", force: :cascade do |t|
+    t.string "attr_type"
+    t.integer "attr_id"
+    t.bigint "lawyer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["lawyer_id"], name: "index_lawyer_attributes_on_lawyer_id"
+  end
+
+  create_table "lawyers", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "short_desc"
+    t.string "long_desc"
+    t.string "email"
+    t.string "phone"
+    t.string "address"
+    t.string "photo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_lawyers_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "conversation_id"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "payment_types", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.integer "stars"
+    t.bigint "booking_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.bigint "conversation_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_subscriptions_on_conversation_id"
+    t.index ["user_id"], name: "index_subscriptions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +117,13 @@ ActiveRecord::Schema.define(version: 2018_11_14_012502) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "lawyers"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "lawyer_attributes", "lawyers"
+  add_foreign_key "lawyers", "users"
+  add_foreign_key "messages", "conversations"
+  add_foreign_key "messages", "users"
+  add_foreign_key "reviews", "bookings"
+  add_foreign_key "subscriptions", "conversations"
+  add_foreign_key "subscriptions", "users"
 end
