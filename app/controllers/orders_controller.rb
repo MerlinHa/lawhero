ddf4061d374e0410d10958b8bea6ba3a06632531
Merlin Hannemann <merlinhannemann@gmx.de>
2018-11-16 18:50:59 +0800

    set up controller and model

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,8 @@
 class OrdersController < ApplicationController
+  def create
+  lawyer = Lawyer.find(params[:lawyer_id])
+  order  = Order.create!(lawyer_sku: lawyer.sku, amount: lawyer.price, state: 'pending', user: current_user)
+
+  redirect_to new_order_payment_path(order)
+  end
 end
