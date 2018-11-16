1f94aed70226c61f7b11277e5331e6e384cf27da
Merlin Hannemann <merlinhannemann@gmx.de>
2018-11-16 13:44:35 +0800

    Fixed relationships for booking and reviews

--- app/models/lawyer.rb
+++ app/models/lawyer.rb
@@ -6,6 +6,8 @@ class Lawyer < ApplicationRecord
   has_many :law_fields
   has_many :payment_types
   has_many :communications
+  has_many :bookings
+  has_many :reviews, through: :bookings
   #has_many :languages, through: :lawyer_properties, source: :property_id, source_type: 'Language'
   # has_many :law_fields, through: :lawyer_properties, source: :property_id, source_type: 'LawField'
   # has_many :law_fields, through: :lawyer_properties, source: :property_id, source_type: 'LawField'
