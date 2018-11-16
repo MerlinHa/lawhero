61d1746812d9213c34cfada2f3f95737e9c3ca86
Merlin Hannemann <merlinhannemann@gmx.de>
2018-11-16 19:06:50 +0800

    payment process is working except show order page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_16_104043) do
+ActiveRecord::Schema.define(version: 2018_11_16_105617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 2018_11_16_104043) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "price_cents", default: 0, null: false
+    t.string "sku"
     t.index ["user_id"], name: "index_lawyers_on_user_id"
   end
 
