ca967897c19a511b93bed470a5f2313a53988cd3
Shawkat Shami <shawkat.shami7@gmail.com>
2018-11-16 15:16:52 +0800

    latest update to index frontend

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_14_074642) do
+ActiveRecord::Schema.define(version: 2018_11_15_011217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,16 @@ ActiveRecord::Schema.define(version: 2018_11_14_074642) do
     t.datetime "updated_at", null: false
     t.index ["lawyer_id"], name: "index_bookings_on_lawyer_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "cases", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "lawyer_id"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["lawyer_id"], name: "index_cases_on_lawyer_id"
+    t.index ["user_id"], name: "index_cases_on_user_id"
   end
 
   create_table "communications", force: :cascade do |t|
@@ -119,6 +129,8 @@ ActiveRecord::Schema.define(version: 2018_11_14_074642) do
 
   add_foreign_key "bookings", "lawyers"
   add_foreign_key "bookings", "users"
+  add_foreign_key "cases", "lawyers"
+  add_foreign_key "cases", "users"
   add_foreign_key "lawyer_attributes", "lawyers"
   add_foreign_key "lawyers", "users"
   add_foreign_key "messages", "conversations"
