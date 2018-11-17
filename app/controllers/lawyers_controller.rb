12086b18217e3b61f078c3144bbc00f48d27b689
Shawkat Shami <shawkat.shami7@gmail.com>
2018-11-17 14:16:11 +0800

    filter search for lawyers index

--- app/controllers/lawyers_controller.rb
+++ app/controllers/lawyers_controller.rb
@@ -1,10 +1,10 @@
 class LawyersController < ApplicationController
   def index
     @lawyers = Lawyer.all
+    ### make it work for not one filter, but more than one
   end
 
   def show
     @lawyer = Lawyer.find(params[:id])
   end
-
 end
