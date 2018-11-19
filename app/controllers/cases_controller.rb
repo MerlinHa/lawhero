581830003ce6bc16243abe9213ee2790d6697d8d
Merlin Hannemann <merlinhannemann@gmx.de>
2018-11-19 11:03:53 +0800

    added request controller stuff to case controller

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -6,12 +6,28 @@ class CasesController < ApplicationController
   end
 
   def new
-    @case = Case.new
+    @case = Case.new(params_cases)
+    @lawyer = Lawyer.find(params[:lawyer_id])
+    @case.lawyer = @lawyer
+    @case.user = current_user
+    if @case.save
+      redirect_to new_order_payment_path(@case)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @case = Case.find(params[:id])
+  end
+
+  def index
+    @cases = current_user.cases
+    @user = current_user
   end
 
   def create
     @case = Case.new(params_cases)
-    # @case.request = @request
     @case.save
     redirect_to cases_path
   end
