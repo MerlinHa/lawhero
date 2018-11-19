2aaa5e4082a098d2d339f97f8d3a14f6449bdf34
Merlin Hannemann <merlinhannemann@gmx.de>
2018-11-19 11:06:05 +0800

    changed order model

--- app/models/case.rb
+++ app/models/case.rb
@@ -3,9 +3,13 @@ class Case < ApplicationRecord
   belongs_to :lawyer
   mount_uploaders :documents, PhotoUploader
 
+  def add_order
+    Order.create(request_id: self.id, lawyer_sku: self.lawyer.sku, amount: self.lawyer.price, user: self.user, state: 'pending')
+  end
+
   private
 
   def case_params
-    params.require(:case).permit(:user_id, :lawyer_id, :status, :title, :description, :request_id, {documents: []})
+    params.require(:case).permit(:user_id, :lawyer_id, :status, :title, :description, {documents: []})
   end
 end
