5baa0f676c508df8a46cd849b93cd2718baa32e8
Shawkat Shami <shawkat.shami7@gmail.com>
2018-11-19 10:06:16 +0800

    filter dropwdown added and clickable lawyer mini cards fixed

--- app/controllers/lawyers_controller.rb
+++ app/controllers/lawyers_controller.rb
@@ -1,18 +1,11 @@
 class LawyersController < ApplicationController
   def index
     @lawyers = Lawyer.all
-       if params.key?("law_field")
-      law_field = params[:law_field]
-      language = params[:language]
-      # @lawyers = Lawyer.joins(:lawyer_properties).joins(:law_fields).where("law_fields.content = ?", law_field).or(Lawyer.joins(:lawyer_properties).joins(:languages).where("languages.content = ?", language))
-      @lawyers = Lawyer.joins(:lawyer_properties).joins(:law_fields).joins(:languages).where("law_fields.content = ? AND languages.content = ?", law_field, language).uniq
-      # law_field = params[:law_field]
-      # @lawyers = Lawyer.joins(:lawyer_properties).joins(:law_fields).where("law_fields.content = ?", law_field)
-      # @lawyers = @lawyers.uniq
-      # language = params[:language]
-      # @lawyers = @lawyers.joins(:lawyer_properties).joins(:languages).where("languages.content = ?", language)
-      # @lawyers = @lawyers.uniq
-    end
+      if params.key?("law_field")
+        law_field = params[:law_field]
+        language = params[:language]
+        @lawyers = Lawyer.joins(:lawyer_properties).joins(:law_fields).joins(:languages).where("law_fields.content = ? AND languages.content = ?", law_field, language).uniq
+      end
   end
 
   def show
