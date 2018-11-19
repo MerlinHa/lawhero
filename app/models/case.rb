68922848f1a22e44cc963c1dc51b5585c09e4d8e
Merlin Hannemann <merlinhannemann@gmx.de>
2018-11-19 10:38:14 +0800

    added document uploadedr, not completely finished

--- app/models/case.rb
+++ app/models/case.rb
@@ -3,4 +3,10 @@ class Case < ApplicationRecord
   belongs_to :lawyer
   belongs_to :request
   mount_uploaders :documents, PhotoUploader
+
+  private
+
+  def case_params
+    params.require(:case).permit(:user_id, :lawyer_id, :status, :title, :description, :request_id, {documents: []})
+  end
 end
