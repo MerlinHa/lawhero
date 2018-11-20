22a15a3c36557ca935ef4e9ed3394731623b53e8
Paul Guadagnin <paul.guadagnin@gmail.com>
2018-11-20 15:03:24 +0700

    filter addition

--- app/controllers/lawyers_controller.rb
+++ app/controllers/lawyers_controller.rb
@@ -1,13 +1,14 @@
 class LawyersController < ApplicationController
   def index
     @lawyers = Lawyer.all
-
-
       if params.key?("law_field")
         law_field = params[:law_field]
         language = params[:language]
+        payment_type = params[:payment_type]
 
-        @lawyers = Lawyer.joins(:lawyer_properties).joins(:law_fields).joins(:languages).where("law_fields.content = ? AND languages.content = ?", law_field, language).uniq
+        # @lawyers = Lawyer.joins(:lawyer_properties).joins(:law_fields).joins(:languages).where("law_fields.content = ? AND languages.content = ?", law_field, language).uniq
+
+      @lawyers = Lawyer.joins(:lawyer_properties).joins(:law_fields).joins(:languages).joins(:payment_types).where(law_field, language, payment_type).uniq
       end
   end
 
