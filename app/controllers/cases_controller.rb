11c4898dff71a88f987057f6e660f98e80b0dd83
Shawkat Shami <shawkat.shami7@gmail.com>
2018-11-21 11:04:25 +0800

    re added the messaging feature, css needs work

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -3,12 +3,12 @@ class CasesController < ApplicationController
   def index
     @cases = current_user.cases
     @user = current_user
-    @message = Message.new
   end
 
   def show
     @case = Case.find(params[:id])
     @user = current_user
+    @message = Message.new
   end
 
 
