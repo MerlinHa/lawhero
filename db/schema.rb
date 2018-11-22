ce8a5f441f1d7a790f6e282b66c422c690ccf0cf
Paul Guadagnin <paul.guadagnin@gmail.com>
2018-11-22 08:52:59 +0700

    migration cases

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_21_073404) do
+ActiveRecord::Schema.define(version: 2018_11_22_014714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,9 @@ ActiveRecord::Schema.define(version: 2018_11_21_073404) do
     t.datetime "updated_at", null: false
     t.string "title"
     t.string "description"
+    t.integer "financial_cost", default: 1000
+    t.integer "overall_length", default: 180
+    t.float "odds_success", default: 0.5
     t.index ["lawyer_id"], name: "index_cases_on_lawyer_id"
     t.index ["user_id"], name: "index_cases_on_user_id"
   end
