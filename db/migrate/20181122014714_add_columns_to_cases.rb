b096c98ef30f1131f0c56196e9f349075264729b
Paul Guadagnin <paul.guadagnin@gmail.com>
2018-11-22 12:09:27 +0700

    fixed logic overview

--- db/migrate/20181122014714_add_columns_to_cases.rb
+++ db/migrate/20181122014714_add_columns_to_cases.rb
@@ -2,6 +2,6 @@ class AddColumnsToCases < ActiveRecord::Migration[5.2]
   def change
     add_column :cases, :financial_cost, :integer, default: 1000
     add_column :cases, :overall_length, :integer, default: 180
-    add_column :cases, :odds_success, :float, default: 0.5
+    add_column :cases, :odds_success, :integer, default: 50
   end
 end
